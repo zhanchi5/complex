305a839470293edf4e681e92b12d791c99fba629
zhanchi5 <zhanchi5@outlook.com>
2017-05-18 22:13:34 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -11,3 +11,63 @@ SCENARIO("testing +"){
   Complex temp = a + b;
   REQUIRE (temp == c);
 }
+SCENARIO("testing +="){
+  Complex a(5,2);
+  Complex b(3,-3);
+  
+  Complex c(18,-9);
+  Complex temp = a+=b;
+  REQUIRE (temp == c);
+}
+SCENARIO("testing *"){
+  Complex a(5,2);
+  Complex b(3,-3);
+  
+  Complex c(21,-9);
+  
+  Complex temp = a*b;
+  REQUIRE (temp == c);
+}
+SCENARIO("testing *="){
+  Complex a(5,2);
+  Complex b(3,-3);
+  
+  Complex c(15,-6);
+  Complex temp = a*=b;
+  REQUIRE (temp == c);
+  } 
+
+SCENARIO("testing -"){
+  Complex a(5,2);
+  Complex b(3,-3);
+  
+  Complex c(2,5);
+  
+  Complex temp = a-b;
+  REQUIRE (temp == c);
+}
+SCENARIO("testing -="){
+  Complex a(5,2);
+  Complex b(3,-3);
+  
+  Complex c(15,-6);
+  Complex temp = a-=b;
+  REQUIRE (temp == c);
+
+SCENARIO("testing /"){
+  Complex a(5,2);
+  Complex b(3,-3);
+  
+  Complex c(0.5, 1.6667);
+  Complex temp = a/b;
+  REQUIRE (temp == c);
+}
+  
+SCENARIO("testing /="){
+  Complex a(5,2);
+  Complex b(3,-3);
+  
+  Complex c(5,2);
+  Complex temp = a/=b;
+  REQUIRE (temp == c);  
+  
